651e071f314841255740522f039d95953abfbb83
kapiyanov <fafahyhy@gmail.com>
2022-06-02 01:40:02 +0600

    new: setup config for prod

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const {Sequelize} = require('sequelize')
 
 module.exports = new Sequelize(
-    process.env.DB_NAME, // Название БД
-    process.env.DB_USER, // Пользователь
-    process.env.DB_PASSWORD, // ПАРОЛЬ
+    process.env.DB_NAME || 'mezhdusoboicheck', // Название БД
+    process.env.DB_USER || 'postgres', // Пользователь
+    process.env.DB_PASSWORD || 'root', // ПАРОЛЬ
     {
         dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || '5432'
     }
 )
