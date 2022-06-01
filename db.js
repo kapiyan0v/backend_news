2efef9e86b12e86d57efc56405631bddee02ab8c
kapiyanov <fafahyhy@gmail.com>
2022-06-02 01:25:07 +0600

    new: setup config for prod

--- db.js
+++ db.js
@@ -1,19 +1,12 @@
-const {Sequelize} = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-);
+const {Sequelize} = require('sequelize')
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+module.exports = new Sequelize(
+    process.env.DB_NAME, // Название БД
+    process.env.DB_USER, // Пользователь
+    process.env.DB_PASSWORD, // ПАРОЛЬ
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
+    }
+)
