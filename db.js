b63e212cfa60dc774a3f56a7d549915f9f6fdffe
kapiyanov <fafahyhy@gmail.com>
2022-06-01 06:01:43 +0600

    upd

--- db.js
+++ db.js
@@ -14,4 +14,4 @@ sequelize.authenticate().then(() => {
     console.log('Connection has been established successfully.');
   }).catch(err => {
     console.error('Unable to connect to the database:', err);
-  });
+  }).done();
