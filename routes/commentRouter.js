b795341151f92c21e8771a59f3baaacbe8fdd1b3
kapiyanov <fafahyhy@gmail.com>
2022-06-05 23:15:58 +0600

    upd

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,10 +1,10 @@
 const Router = require('express')
 const router = new Router()
-const commentsRouter = require('../controllers/commentController')
+const commentController = require('../controllers/commentController')
 
-router.get('/', commentsRouter.get)
-router.post('/', commentsRouter.create)
-router.delete('/:id', commentsRouter.delete)
-router.patch('/', commentsRouter.edit)
+router.get('/', commentController.get)
+router.post('/', commentController.create)
+router.delete('/:id', commentController.delete)
+router.patch('/', commentController.edit)
 
 module.exports = router
